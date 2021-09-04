933a9c138f00904f075aaeaaa381bfa84187bc00
MichaelCND <mffm80580@gmail.com>
2021-09-04 19:39:19 +0300

    1

---
+++ Project1 cpp/Project1 cpp/Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	string st = "moi privetstviya, ";
+	int num_1, num_2, result;
+
+	cout << "this is addition, enter first NUMBER";
+	cout << "\n";
+	cin >> num_1;
+	cout << "\n";
+	cout << "enter first SECOND";
+	cout << "\n";
+	cin >> num_2;
+	cout << "\n";
+	result = num_1 + num_2;
+	cout << st << "result is " << result;
+
+	return 0;
+}
