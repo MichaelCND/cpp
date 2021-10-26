e7a140b5f4defa15b6bbdff2d2b036bd8e3504ce
MichaelCND <mffm80580@gmail.com>
2021-10-26 21:12:37 +0300

    вложенный цикл

---
+++ table/table/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int res = 0;
+	int col;
+	int row;
+
+	cout << "colichestvo liniy";
+	cin >> col;
+	cout << "colichestvo stolbtsov";
+	cin >> row;
+
+	for (int i = 0; i < col; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+			res++;
+			cout << res;
+		}
+		cout << endl; 
+	}
+	return 0;
+	
+}
