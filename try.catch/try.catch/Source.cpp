e23f0d2a9c5db324f219a3376b7a855227435094
MichaelCND <mffm80580@gmail.com>
2021-10-26 12:46:21 +0300

    1

---
+++ try.catch/try.catch/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	//   а    задекларированная
+	int b = 0;
+	//   в    проинециализированная
+	int c;
+
+	try
+	{
+		cout << "1 chislo";
+		cin >> a;
+		cout << "2 choslo";
+		cin >> b;
+
+		c = a / b;
+		cout << c;
+	}
+	catch (const std::exception& ex)
+	{
+		cout << ex.what();
+	}
+
+	return 0;
+}
